1d371701a53d01b69426265e613d2b03ce950417
Nikita <poberezhnyuk.n.i@edu.mirea.ru>
2023-10-12 20:21:00 +0300

    Initial commit

---
+++ src/test/java/mirea/JWTAuthExample/JwtAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package mirea.JWTAuthExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
